22062dd1925db41def38ab100723f66c49970404
hazzoum <hazthe12@gmail.com>
2021-07-27 11:41:29 -0700

    tasks

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+#include <unistd.h>
+/* functions prototype */
+int _printf(const char *format, ...);
+int _putchar(char c);
+/* functions prototypes conversion */
+int print_char(char arg);
+int print_string(char *arg);
+int print_int(int arg);
+#endif/* PRINTF_H */
