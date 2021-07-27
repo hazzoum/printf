79d79af5dee9bc5ee9166d895574b86fa7297df6
hazzoum <hazthe12@gmail.com>
2021-07-27 11:48:01 -0700

    task 0

--- _printf.c
+++ _printf.c
@@ -4,50 +4,38 @@
  * @format: format specifier
  * Return: count
  */
-int _printf(const char *format, ...) {
-va_list arg;
-va_start(arg,format);
-int i = 0;
-int len = 0;
-while(format[i]!='\0')
+int _printf(const char *format, ...)
 {
-if(format[i] == '%'){
-if(format[i+1] == 'd' || format[i+1] == 'i'){
-len = len + print_int((int)va_arg(arg,int));
-i = i + 2;
-}
-else if(format[i+1] == 'c'){
-len = len + print_char((char)va_arg(arg,int));
-i = i + 2;
-}
-else if(format[i+1] == 's'){
-len = len + print_string(va_arg(arg,char *));
-i = i + 2;
-}
-else if(format[i+1] == 'b'){
-len = len + print_int(tobin(va_arg(arg,int)));
-i = i + 2;
-}
-else if(format[i+1] == 'o'){
-len = len + print_int(tooct(va_arg(arg,int)));
-i = i + 2;
-}
-else if(format[i+1] == 'X'){
-len = len + print_int(toHEX(va_arg(arg,int)));
-i = i + 2;
-}
-else if(format[i+1] == 'x'){
-len = len + print_int(tohex(va_arg(arg,int)));
-i = i + 2;
-}
-else if(format[i+1] == 'p'){
-len = len + print_pointer(va_arg(arg,unsigned long));
-i = i + 2;
-}
-} 
-else 
-len = len + print_char(format[i]);
-i++;
-}
-return (len);
+	va_list arg;
+	int i = 0, count = 0, fun = 0;
+
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
+	va_start(arg, format);
+	while (*(format + i) && format)
+	{
+		if (*(format + i) != '%')
+		{
+			_putchar (*(format + i));
+			count++;
+		}
+		if (*(format + i) == '%')
+		{
+			fun = get_func(*(format + (i + 1)), arg);
+			if (fun != 0)
+			{
+				count = count + fun;
+				i = i + 2;
+				continue;
+			}
+			if (*(format + (i + 1)) == '\0')
+			{
+				_putchar(*(format + i));
+				count++;
+			}
+		}
+			i++;
+	}
+	va_end(arg);
+	return (count);
 }
