26ddb297501aa61baec035bfce5836f73dc78dd6
hazzoum <hazthe12@gmail.com>
2021-07-30 09:55:51 -0700

    tasks

---
+++ get_func.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * get_func - check depending on separator
+ * @s: char
+ *@ap: argument of type va_list
+ * Return: int
+ */
+int get_func(const char s, va_list ap)
+{
+	int i = 0, count = 0;
+	printf_t ch[] = {
+		{'c', print_char},
+		{'s', print_string},
+		{'%', print_percent},
+		{'d', print_int},
+		{'i', print_int},
+		{'b', print_binaire},
+		{'u', print_unsf},
+		{'o', print_oct},
+		{'x', print_hex},
+		{'X', print_heX},
+		{'R', _rot13},
+		{'r', _printrev},
+		{'p', print_P}
+	};
+	while (ch[i].c != 0)
+	{
+		if (ch[i].c == s)
+		{
+			count = count + ch[i].fun(ap);
+			return (count);
+		}
+		i++;
+	}
+	return (0);
+}
+
